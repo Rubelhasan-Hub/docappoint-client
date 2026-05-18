4303f0ac9da1e22eec0803a2845257dc9b3e883f
Rubelhasan29 <rubelhasanmd29@gmail.com>
2026-05-18 09:58:13 +0600

    added the Toggling and login register page

--- src/components/MoreSection.jsx
+++ src/components/MoreSection.jsx
@@ -1,39 +1,42 @@
 import { Card } from "@heroui/react";
 import Link from "next/link";
-import { BiRightArrowAlt, BiUser } from "react-icons/bi";
+import { BiBookBookmark, BiRightArrowAlt, BiUser } from "react-icons/bi";
+import { GrEmergency } from "react-icons/gr";
+import { MdOnlinePrediction } from "react-icons/md";
+
 
 const MoreSection = () => {
     return (
-        <div className="max-w-9/12 mx-auto mt-30 mb-20"  >
+        <div className="max-w-9/12 mx-auto mt-10 mb-20 lg:mt-30"  >
             <div>
                 <h2 className="text-center text-4xl font-semibold">Our Services</h2>
                 <h2 className="text-center text-lg">We provide a wide range of medical services to meet your needs</h2>
-                <div className="flex justify-between mt-7">
-                    <Card className="w-[320px] shadow-2xl p-10 space-y-1" variant="transparent">
+                <div className=" space-y-10 lg:space-y-0 lg:flex justify-between mt-7">
+                    <Card className="w-[320px] shadow-2xl p-10 space-y-1 flex justify-between hover:translate-y-1 transition duration-300" variant="transparent">
                         <Card.Header>
-                            <BiUser className="text-6xl p-4 bg-green-200 m-2 rounded-3xl"/>
-                            <Card.Title className="text-2xl">Find Doctors</Card.Title>
-                            <Card.Description>Easily Find Qualified Doctors in your Area</Card.Description>
+                            <MdOnlinePrediction className="text-6xl p-4 bg-green-200 m-2 rounded-3xl"/>
+                            <Card.Title className="text-2xl">Online Consultation</Card.Title>
+                            <Card.Description>Get professional medical advice from certified doctors anytime through secure online consultation.</Card.Description>
                         </Card.Header>
                         <Link href="/" className="text-green-500 flex items-center gap-2"><span className="border-b-2 border-green-500">Read More</span> <BiRightArrowAlt /></Link>
                     </Card>
-                    <Card className="w-[320px] shadow-2xl p-10 space-y-1" variant="transparent">
+                    <Card className="w-[320px] shadow-2xl p-10 space-y-1 flex justify-between hover:translate-y-1 transition duration-300" variant="transparent">
                         <Card.Header>
-                            <BiUser className="text-6xl p-4 bg-green-200 m-2 rounded-3xl"/>
-                            <Card.Title className="text-2xl">Find Doctors</Card.Title>
-                            <Card.Description>Easily Find Qualified Doctors in your Area</Card.Description>
+                            <BiBookBookmark className="text-6xl p-4 bg-green-200 m-2 rounded-3xl"/>
+                            <Card.Title className="text-2xl">Appointment Booking</Card.Title>
+                            <Card.Description>Book doctor appointments quickly and conveniently without waiting in long queues.</Card.Description>
                         </Card.Header>
                         <Link href="/" className="text-green-500 flex items-center gap-2"><span className="border-b-2 border-green-500">Read More</span> <BiRightArrowAlt /></Link>
                     </Card>
-                    <Card className="w-[320px] shadow-2xl p-10 space-y-1" variant="transparent">
+                    <Card className="w-[320px] shadow-2xl p-10 space-y-1 flex justify-between hover:translate-y-1 transition duration-300" variant="transparent">
                         <Card.Header>
-                            <BiUser className="text-6xl p-4 bg-green-200 m-2 rounded-3xl"/>
-                            <Card.Title className="text-2xl">Find Doctors</Card.Title>
-                            <Card.Description>Easily Find Qualified Doctors in your Area</Card.Description>
+                            <GrEmergency className="text-6xl p-4 bg-green-200 m-2 rounded-3xl"/>
+                            <Card.Title className="text-2xl">Emergency Support</Card.Title>
+                            <Card.Description>Access immediate medical assistance and emergency healthcare support whenever you need.</Card.Description>
                         </Card.Header>
                         <Link href="/" className="text-green-500 flex items-center gap-2"><span className="border-b-2 border-green-500">Read More</span> <BiRightArrowAlt /></Link>
                     </Card>
-                    <Card className="w-[320px] shadow-2xl p-10 space-y-1" variant="transparent">
+                    <Card className="w-[320px] shadow-2xl p-10 space-y-1 flex justify-between hover:translate-y-1 transition duration-300" variant="transparent">
                         <Card.Header>
                             <BiUser className="text-6xl p-4 bg-green-200 m-2 rounded-3xl"/>
                             <Card.Title className="text-2xl">Find Doctors</Card.Title>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,55 @@
+"use client"
 import { Button } from "@heroui/react";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { FiMenu } from "react-icons/fi";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const pathName = usePathname()
+
     return (
-        <div className="shadow-lg">
+        <div className="shadow-lg sticky top-0 z-50 bg-white">
             <nav className="flex justify-between  max-w-11/12 mx-auto items-center p-5">
                 <div className="flex items-center">
                     <Image className="w-14" src="/assets/54-545682_doctor-logo-doctor-logo-png.png" alt="DocAppointment Logo" width={50} height={50} loading="eager"></Image>
-                    <h2 className="text-3xl font-semibold"><span className="text-green-600">Doc</span>Appointment</h2>
+                    <h2 className="text-2xl lg:text-3xl font-semibold"><span className="text-green-600">Doc</span>Appointment</h2>
                 </div>
                 <div>
-                    <ul className="flex gap-7 text-lg">
-                        <li><Link href="/">Home</Link></li>
-                        <li><Link href="/">All Appointments</Link></li>
-                        <li><Link href="/">Dashboard</Link></li>
+                    <ul className="hidden lg:flex gap-7 text-lg ">
+                        <li><Link href="/" className={pathName === "/" ? "border-b-2 border-green-600" : "border-0"}>Home</Link></li>
+                        <li><Link href="/all-appointment" className={pathName === "/all-appointment" ? "border-b-2 border-green-600" : "border-0"}>All Appointments</Link></li>
+                        <li><Link href="/dashboard" className={pathName === "/dashboard" ? "border-b-2 border-green-600" : "border-0"}>Dashboard</Link></li>
                     </ul>
                 </div>
-                <div className="flex gap-3">
-                    <Button variant="outline">Login</Button>
-                    <Button className="bg-green-600">Register</Button>
+                <div className="hidden gap-3 lg:flex ">
+                    <Link href="/login"><Button variant="outline" className="hover:bg-blue-100">Login</Button></Link>
+                    <Link href="/register"><Button className="bg-green-600 hover:bg-green-700">Register</Button></Link>
                 </div>
+
+                <button onClick={() => setIsMobileMenuOpen((currentValue) => !currentValue)} className="md:hidden p-2 text-foreground">
+                    {isMobileMenuOpen ? <RxCross2 size={24} /> : <FiMenu size={24} />}
+                </button>
+
             </nav>
+            {isMobileMenuOpen && (<div className="md:hidden bg-green-100 rounded-2xl">
+                <div className="container mx-auto px-6 py-6 flex flex-col gap-4 items-end">
+                    <div>
+                        <ul className="space-y-4 text-lg ">
+                            <li><Link href="/">Home</Link></li>
+                            <li><Link href="/">All Appointments</Link></li>
+                            <li><Link href="/">Dashboard</Link></li>
+                        </ul>
+                    </div>
+                    <div className="flex gap-1">
+                        <Button variant="outline" >Login</Button>
+                        <Button className="bg-green-600">Register</Button>
+                    </div>
+                </div>
+            </div>)}
         </div>
     );
 };
