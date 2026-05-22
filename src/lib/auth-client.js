4ce9e0aacddc95d980d6dd4000969b9560e58612
Rubelhasan29 <rubelhasanmd29@gmail.com>
2026-05-22 10:32:40 +0600

    fix: export from authClient not new createAuthClient

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,4 +1,4 @@
-import { jwtClient } from "better-auth/client/plugins"
+((import { jwtClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
@@ -6,4 +6,4 @@ export const authClient = createAuthClient({
     plugins: [jwtClient()]
 })
 
-export const { signIn, signUp, useSession, signOut } = createAuthClient()
+export const { signIn, signUp, useSession, signOut } = authClient
