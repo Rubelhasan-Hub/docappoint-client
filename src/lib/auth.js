5fdc4085df2310e68e471905403b4c86d518262c
Rubelhasan29 <rubelhasanmd29@gmail.com>
2026-05-22 10:20:05 +0600

    fix: add baseURL and secret to better-auth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -9,6 +9,8 @@ const db = client.db("docappoinment");
 
 
 export const auth = betterAuth({
+    baseURL: process.env.BETTER_AUTH_URL,
+    secret: process.env.BETTER_AUTH_SECRET,
     database: mongodbAdapter(db, {
         // Optional: if you don't provide a client, database transactions won't be enabled.
         client
